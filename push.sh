4d6a8e73bfd03e1eef596cb532b5994e61a66ede
kheshav Sewnundun <kheshavsewnundun@kheshavs-MacBook-Air.local>
2022-01-16 21:07:43 +0400

    package deployment name #2

--- push.sh
+++ push.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# Based on Environment set URL
 if [[ "$CI_COMMIT_BRANCH" == "master" ]]; then
     environment="uat"
 fi
@@ -10,6 +9,8 @@ if [[ "$CI_COMMIT_BRANCH" == "master" && "$CI_COMMIT_TAG" == null ]]; then
 fi
 
 url="http://api.automatdeck-${environment}.svc.cluster.local:8000/62268f76fff54aea8aac44a0fd9a28c9/"
+
+cd build
 
 for FILE in **/*
 do
