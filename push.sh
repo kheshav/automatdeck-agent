3c6b34cc9a0b668214fba79cf9a10549364e8cd4
kheshav Sewnundun <kheshavsewnundun@kheshavs-MacBook-Air.local>
2022-01-19 20:56:30 +0400

    debug

--- push.sh
+++ push.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [[ "$CI_COMMIT_BRANCH" == "master" ]]; then
-    environment="uat"
+    mytarget="uat"
 fi
 
 if [[ "$CI_COMMIT_BRANCH" == "master" && "$CI_COMMIT_TAG" == null ]]; then
-    url="prod"
+    mytarget="prod"
 fi
 
-url="http://api.automatdeck-${environment}.svc.cluster.local:8000/62268f76fff54aea8aac44a0fd9a28c9/"
+url="http://api.automatdeck-${mytarget}.svc.cluster.local:8000/62268f76fff54aea8aac44a0fd9a28c9/"
 
 cd build
 
@@ -19,6 +19,6 @@ do
     IFS="/"; declare -a Array=($*)
     echo "Arch: ${Array[0]}" 
     echo "File: ${Array[1]}"
-    curl -F "name=${Array[1]}" -F "package=@$FILE" -H "access_key:$repo_access_key" -H "secret_key:$repo_secret_key" $url
-    curl -F "name=ad-agent_latest_${Array[0]}.tar.gz" -F "package=@$FILE" -H "access_key:$repo_access_key" -H "secret_key:$repo_secret_key" $url
+    curl -F "name=${Array[1]}" -F "package=@$FILE" -H "access_key:$repo_access_key" -H "secret_key:$repo_secret_key" "$url" -vv
+    curl -F "name=ad-agent_latest_${Array[0]}.tar.gz" -F "package=@$FILE" -H "access_key:$repo_access_key" -H "secret_key:$repo_secret_key" "$url" -vv
 done    
